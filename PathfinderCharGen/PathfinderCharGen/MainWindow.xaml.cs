01637c86d38c696f23b70227a35a29c06f08905c
Saltylanguage <joshcampbell1986@gmail.com>
2016-11-27 05:00:25 -0800

    View separation working!  Continuing work on ViewModel and Data Binding

--- PathfinderCharGen/PathfinderCharGen/MainWindow.xaml.cs
+++ PathfinderCharGen/PathfinderCharGen/MainWindow.xaml.cs
@@ -32,47 +32,32 @@ namespace PathfinderCharGen
             InitializeComponent();
         }
 
-        private void MaxHP_TextChanged(object sender, TextChangedEventArgs e)
-        {
+        //private void MenuItem_Click_Save(object sender, RoutedEventArgs e)
+        //{
+        //    //This doesn't work yet
+        //    SaveFileDialog saveFileDialog = new SaveFileDialog();
+        //    saveFileDialog.Filter = "Json file (*.json) [recomended]|*.json"; //|Xml file (*.xml)|*.xml
+        //    if (saveFileDialog.ShowDialog() == true)
+        //    {
+        //        string json = JsonConvert.SerializeObject(hero);
+        //        System.IO.File.WriteAllText(saveFileDialog.FileName, json);
 
-        }
+        //        int stop = 0;
+        //    }
+        //}
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e) //TODO figure out how to name this one. (THIS IS HP)
-        {
+        //private void MenuItem_Click_Load(object sender, RoutedEventArgs e)
+        //{
+        //    //This doesn't work yet
+        //    OpenFileDialog openFileDialog = new OpenFileDialog();
+        //    openFileDialog.Filter = "Json file (*.json)|*.json";
+        //    if (openFileDialog.ShowDialog() == true)
+        //    {
+        //        string json = System.IO.File.ReadAllText(openFileDialog.FileName);
+        //        hero = JsonConvert.DeserializeObject<Character>(json);
 
-        }
-
-        private void AC_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void MenuItem_Click_Save(object sender, RoutedEventArgs e)
-        {
-            //This doesn't work yet
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Json file (*.json) [recomended]|*.json"; //|Xml file (*.xml)|*.xml
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                string json = JsonConvert.SerializeObject(hero);
-                System.IO.File.WriteAllText(saveFileDialog.FileName, json);
-
-                int stop = 0;
-            }
-        }
-
-        private void MenuItem_Click_Load(object sender, RoutedEventArgs e)
-        {
-            //This doesn't work yet
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Json file (*.json)|*.json";
-            if (openFileDialog.ShowDialog() == true)
-            {
-                string json = System.IO.File.ReadAllText(openFileDialog.FileName);
-                hero = JsonConvert.DeserializeObject<Character>(json);
-
-                int stop = 0;
-            }
-        }
+        //        int stop = 0;
+        //    }
+        //}
     }
 }
---
+++ PathfinderCharGen/PathfinderCharGen/Views/CharGenView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PathfinderCharGen.Views
+{
+    /// <summary>
+    /// Interaction logic for CharGenView.xaml
+    /// </summary>
+    public partial class CharGenView : UserControl
+    {
+        public CharGenView()
+        {
+            InitializeComponent();
+        }
+    }
+}
